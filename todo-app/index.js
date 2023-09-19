50d176259260a848066b2022dbb10517e5b5fe09
Shreyas <shreyasdeshingkar@gmail.com>
2023-09-19 08:56:55 -0400

    level 6 added

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,11 +1,5 @@
-const express = require('express');
-const app = express();
-
-
-app.get("/",  (request, response) => {
-    response.send('Hello World')
-})
-
+/* eslint-disable no-undef */
+const app = require("./app");
 app.listen(3000, () => {
     console.log("Started express server at port 3000")
 })
